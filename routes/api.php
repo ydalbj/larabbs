1ee061759c2eb077d9aafa92a6b0d44d0d74e9f0
liuhui <liuhuidut@sina.com>
2018-03-31 20:47:00 +0800

    user update

--- routes/api.php
+++ routes/api.php
@@ -50,8 +50,17 @@ $api->version('v1', [
 
         // 需要token验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
+            //当期登录用户信息
             $api->get('user', 'UsersController@me')
                 ->name('api.user.show');
+
+            // 图片资源
+            $api->post('images', 'ImagesController@store')
+                ->name('api.images.store');
+
+            // 编辑登录用户信息
+            $api->patch('user', 'UsersController@update')
+                ->name('api.user.update');
         });
     });
 });
