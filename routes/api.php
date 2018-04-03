a08f26aff7b0e196c02ffdc09820ffd0fbceaa4e
liuhui <liuhuidut@sina.com>
2018-04-03 23:52:33 +0800

    topic update api

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array', 'bindings']
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -61,6 +61,14 @@ $api->version('v1', [
             // 图片资源
             $api->post('images', 'ImagesController@store')
                 ->name('api.images.store');
+
+            // 发布话题
+            $api->post('topics', 'TopicsController@store')
+                ->name('api.topics.store');
+
+            // 修改话题
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
 
             // 编辑登录用户信息
             $api->patch('user', 'UsersController@update')
